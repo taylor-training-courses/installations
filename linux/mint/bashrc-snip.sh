49482719a90001d7f85690af0a7e2ba4f4fe2060
Jason Taylor <jason@jasongtaylor.com>
2018-08-27 21:28:18 -0400

    Update bashrc-snip.sh

--- linux/mint/bashrc-snip.sh
+++ linux/mint/bashrc-snip.sh
@@ -1,4 +1,5 @@
 # If Git Prompt exists, setup up prompt
+# Place at bottom of ".bashrc" file
 if [ -f ~/.git-prompt.sh ]; then
     source ~/.git-prompt.sh
     export PS1=' \e[36m\u\e[35m:\e[33m\W\e[32;1m $(__git_ps1 "(%s)") \e[37;1m$ \e[0m'
